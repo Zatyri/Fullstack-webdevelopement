419638430e6ded92e1de7b832b276fcfcab0d74e
Oskar <gustosk@gmail.com>
2020-08-07 20:58:20 +0300

    Tehtävä 1.2
    Tehtävä 1.2

--- Osa1/kurssitiedot/src/index.js
+++ Osa1/kurssitiedot/src/index.js
@@ -9,12 +9,24 @@ const Header = (props) => {
   )
 }
 
-const Content = (props) => {
+const Part = (props) => {
   return (
     <>
       <p>
         {props.part} {props.exercises}
       </p>
+    </>
+  )
+}
+
+const Content = (props) => {
+  return (
+    <>
+      
+      <Part part = {props.part1} exercises={props.exercises1}/>
+      <Part part = {props.part2} exercises={props.exercises2}/>  
+      <Part part = {props.part3} exercises={props.exercises3}/>   
+      
     </>
   )
 }
@@ -39,9 +51,8 @@ const App = () => {
   return (
     <>
       <Header course = {course} />
-      <Content part = {part1} exercises={exercises1} />
-      <Content part = {part2} exercises={exercises2} />
-      <Content part = {part3} exercises={exercises3} />
+      <Content part1 = {part1} exercises1={exercises1} part2 = {part2} exercises2={exercises2} part3 = {part3} exercises3={exercises3}/>
+
       <Total exercisesAll={exercises1 + exercises2 + exercises3} />
     </>
   )

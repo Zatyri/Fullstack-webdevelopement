098a81eac36f5d7b1d075b3f2c1066a947d73600
Oskar <gustosk@gmail.com>
2020-08-16 19:42:51 +0300

    initial commit
    initial commit

---
+++ Osa2/maiden-tiedot/src/App.js
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+import Countries from './Components/Countries'
+
+const App = () => {
+  return (
+    <>
+      <Countries />
+    </>
+  )
+}
+
+export default App
+
